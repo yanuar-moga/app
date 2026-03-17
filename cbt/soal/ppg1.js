228c5d0d254339fd8c096c72cd450517bd7cd8ef
MB <oppoa5s.mbcell@gmail.com>
2026-03-17 09:33:27 +0700

    Create ppg1.js

---
+++ cbt/soal/ppg1.js
@@ -0,0 +1,36 @@
+const bankSoal = [
+
+{
+soal:"Perangkat lunak yang mengelola perangkat keras disebut...",
+pilihan:[
+"Aplikasi",
+"Sistem Operasi",
+"Compiler",
+"Driver"
+],
+jawaban:1
+},
+
+{
+soal:"Perangkat untuk memasukkan data ke komputer disebut...",
+pilihan:[
+"Input device",
+"Output device",
+"Storage",
+"CPU"
+],
+jawaban:0
+},
+
+{
+soal:"Contoh sistem operasi adalah...",
+pilihan:[
+"Windows",
+"Word",
+"Excel",
+"PowerPoint"
+],
+jawaban:0
+}
+
+]
